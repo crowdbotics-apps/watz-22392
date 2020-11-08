222514eb3dbcdf681f40e1c0a72feaaf7cffe11c
Crowdbotics <team@crowdbotics.com>
2020-11-08 02:53:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/watz_22392/MainActivity.java
@@ -0,0 +1,15 @@
+package com.watz_22392;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "watz_22392";
+    }
+}
