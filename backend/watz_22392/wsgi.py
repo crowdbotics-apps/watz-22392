9b89a00582c272548d800ef5d2a9ba3f9720f224
Crowdbotics <team@crowdbotics.com>
2020-11-08 02:57:12 +0000

    Update models

--- backend/watz_22392/wsgi.py
+++ backend/watz_22392/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watz_22392.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "watz_22392.settings")
 
 application = get_wsgi_application()
